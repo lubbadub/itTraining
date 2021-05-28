666f0ecd72d2e121c56d61bd660a537e6cff3d82
lubbadub <43811350+lubbadub@users.noreply.github.com>
2021-05-28 14:55:12 +0200

    Delete projetFilRouge directory

--- projetFilRouge/src/com/ib/projetFilRouge/dao/IResponsableDAO.java
+++
@@ -1,7 +0,0 @@
-package com.ib.projetFilRouge.dao;
-
-import com.ib.projetFilRouge.bo.Responsable;
-
-public interface IResponsableDAO extends IDAO<Responsable> {
-
-}
--- projetFilRouge/src/com/ib/projetFilRouge/dao/ISalleDAO.java
+++
@@ -1,7 +0,0 @@
-package com.ib.projetFilRouge.dao;
-
-import com.ib.projetFilRouge.bo.Salle;
-
-public interface ISalleDAO extends IDAO<Salle> {
-
-}
